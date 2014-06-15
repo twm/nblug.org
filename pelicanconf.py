d00b59a28c8d06744cbf1805cda9b87d11a512d6
Tom Most <twm@freecog.net>
2014-06-14 17:37:16 -0700

    Usable config and theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,31 +1,54 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'NBLUG Board Members'
-SITENAME = u'North Bay Linux Users Group'
-SITEURL = ''
+import os
+
+AUTHOR = 'NBLUG Board Members'
+SITENAME = 'North Bay Linux Users Group'
+
+SITEURL = os.environ.get('SITEURL', 'http://new.nblug.org')
+
+MENUITEMS = (
+    ('News', ''),
+    ('Find Our Meetings', 'locations/'),
+    ('About Us', 'about/'),
+    ('Archives', 'articles/'),
+)
 
 TIMEZONE = 'America/Los_Angeles'
+DEFAULT_LANG = 'en'
 
-DEFAULT_LANG = u'en'
+THEME = './theme'
+TYPOGRIFY = True
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
+FEED_ALL_ATOM = 'feed.xml'
 
-# Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+DEFAULT_CATEGORY = 'News'
+FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2}).*'
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+DELETE_OUTPUT_DIRECTORY = True
+DIRECT_TEMPLATES = ('index', 'archives')
 
-DEFAULT_PAGINATION = False
+# Save article pages to subdirectories based on publication year.
+ARTICLE_URL = 'articles/{date:%Y}/{slug}.html'
+ARTICLE_SAVE_AS = 'articles/{date:%Y}/{slug}.html'
+
+# Pages get top-level URLs.
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+
+# Enable yearly archives and an index page for them.
+ARCHIVES_URL = 'articles/'
+ARCHIVES_SAVE_AS = 'articles/index.html'
+YEAR_ARCHIVE_SAVE_AS = 'articles/{date:%Y}/index.html'
+YEAR_ARCHIVE_URL = 'articles/{date:%Y}/'
+
+# Disable page generation for authors and tags.  If you enable this stuff
+# you'll also need to update the templates to include links to these pages.
+TAG_SAVE_AS = ''
+TAGS_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+AUTHORS_SAVE_AS = ''
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
