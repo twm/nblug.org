04aafb18809c32562860a0c8f7b6ca00e604c2b7
Tom Most <twm@freecog.net>
2018-07-28 13:41:32 -0700

    Update site URL for HTTPS

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://nblug.org'
+SITEURL = 'https://nblug.org'
 RELATIVE_URLS = False
 
 DELETE_OUTPUT_DIRECTORY = True
