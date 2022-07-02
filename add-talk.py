cd3623bba603a71e0a625586d10c3e6f632a82a1
Tom Most <twm@freecog.net>
2022-07-02 13:12:26 -0700

    Update add-talk.py

--- add-talk.py
+++ add-talk.py
@@ -1,26 +1,22 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 
 import argparse
-from datetime import date, timedelta
 import os
-from string import Template, ascii_letters, digits
 import sys
-
+from datetime import date, timedelta
+from string import Template, ascii_letters, digits
 
 TUESDAY = 1
 
-TEMPLATE = Template("""\
-Tags: general meeting
-Event: ${date} 7:00 pm to 8:30 pm
-Speaker: ${speaker}
-Location: Sonic
-Author: ${author}
-
-${body}
-""")
+TEMPLATE = Template(
+    """\
+"""
+)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--hackfest', default=False, action='store_true', help="Prefill as a hackfest.")
+parser.add_argument(
+    "--hackfest", default=False, action="store_true", help="Prefill as a hackfest."
+)
 
 
 def sanitize(s):
@@ -31,16 +27,16 @@ def sanitize(s):
     return "-".join("".join(c if c in safe else " " for c in s).lower().split())
 
 
-def next_second_tuesday(today):
+def next_second_tuesday(today: date) -> date:
     """
     >>> next_second_tuesday(date(2015, 3, 1))
-    '2015-03-10'
+    date(2015, 3, 10)
     >>> next_second_tuesday(date(2014, 10, 20))
-    '2014-11-11'
+    date(2014, 11, 11)
     >>> next_second_tuesday(date(2014, 11, 11))
-    '2014-11-11'
+    date(2014, 11, 11)
     >>> next_second_tuesday(date(2014, 12, 29))
-    '2015-01-13'
+    date(2015, 1, 13)
     """
     d = today.replace(day=1)
     tuesdays = 0
@@ -60,7 +56,7 @@ def next_second_tuesday(today):
             if tuesdays >= 2:
                 break
             d = d + timedelta(days=1)
-    return d.isoformat()
+    return d
 
 
 def prompt(query, default=""):
@@ -68,7 +64,7 @@ def prompt(query, default=""):
         suffix = " [{}] ".format(default)
     else:
         suffix = " "
-    return raw_input(query + suffix) or default
+    return input(query + suffix) or default
 
 
 def get_gecos():
@@ -82,15 +78,17 @@ def get_gecos():
     except KeyError:
         return ""
 
-    return passwd[4].split(',')[0]
+    return passwd[4].split(",")[0]
 
 
 def main():
     is_hackfest = parser.parse_args().hackfest
 
-    talk_date = prompt(
-        "What's the date of the talk?",
-        default=next_second_tuesday(date.today()),
+    talk_date = date.fromisoformat(
+        prompt(
+            "What's the date of the talk?",
+            default=next_second_tuesday(date.today()).isoformat(),
+        )
     )
     if is_hackfest:
         title = "Lightning Talks & Hackfest"
@@ -101,38 +99,50 @@ def main():
             "**Hackfest:** Bring your hardware or software project to get help with it or just to show it off. A mix of free tech support, show-and-tell, and idle chat.\n"
         )
     else:
-        title = prompt("What is the title of the talk?")
+        title = prompt(
+            "What is the title of the talk?",
+            default=f"{talk_date:%B} General Meeting",
+        )
         speaker = prompt("Who's the speaker?")
         body = ""
 
     author = prompt("What is *your* name?", default=get_gecos())
 
-    lines = ['Title: {}'.format(title)]
+    lines = [
+        f"Title: {title}",
+        "Tags: general meeting",
+        f"Event: {talk_date:%Y-%m-%d} 7:00 pm to 8:30 pm",
+        f"Speaker: {speaker}" if speaker else None,
+        "Location: Flagship Taproom / Spring Thai",
+        f"Author: {author}",
+        "",
+        body,
+    ]
     if is_hackfest:
         # There are a number of posts with the title of hackfest, so we must
         # give new ones a unique slug to avoid collision.
-        lines.append(date.today().strftime('Slug: hackfest-%Y-%m'))
-    lines.append(TEMPLATE.substitute(date=talk_date, speaker=speaker, author=author, body=body))
-    s = '\n'.join(lines)
-    filepath = 'content/news/{}-{}.md'.format(
-        date.today().strftime('%Y-%m-%d'),
+        lines.append(date.today().strftime("Slug: hackfest-%Y-%m"))
+    lines.append(body)
+    s = "\n".join(filter(None, lines))
+    filepath = "content/news/{}-{}.md".format(
+        date.today().strftime("%Y-%m-%d"),
         sanitize(title),
     )
     if os.path.exists(filepath):
         print("Can't create file; would overwrite {}".format(filepath))
         sys.exit(1)
-    with open(filepath, 'w') as f:
+    with open(filepath, "w") as f:
         f.write(s)
 
-    editor = os.environ.get('EDITOR', 'vim') or 'vim'
+    editor = os.environ.get("EDITOR", "vim") or "vim"
     args = [editor, filepath]
-    if 'vim' in editor:
+    if "vim" in editor:
         # Automatically jump to the last line if we know it's vim.
-        args += ['+{}'.format(len(s.splitlines()))]
+        args += ["+{}".format(len(s.splitlines()))]
 
     sys.stdout.flush()  # Always flush before exec.
     os.execvp(editor, args)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
