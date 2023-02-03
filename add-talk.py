12e3de0ee8bd1d537ae8585977a74c99f23fc80d
Tom Most <twm@freecog.net>
2023-02-02 22:08:50 -0800

    Update the template

--- add-talk.py
+++ add-talk.py
@@ -111,9 +111,11 @@ def main():
     lines = [
         f"Title: {title}",
         "Tags: general meeting",
-        f"Event: {talk_date:%Y-%m-%d} 7:00 pm to 8:30 pm",
+        f"Event: {talk_date:%Y-%m-%d} 6:30 pm to 8:00 pm",
+        # f"Event: {talk_date:%Y-%m-%d} 7:00 pm to 8:30 pm",
         f"Speaker: {speaker}" if speaker else None,
-        "Location: Flagship Taproom / Spring Thai",
+        "Location: Down to Earth Cafe",
+        # "Location: Flagship Taproom / Spring Thai",
         f"Author: {author}",
         "",
         body,
